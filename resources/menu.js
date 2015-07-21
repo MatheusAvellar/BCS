5ed575e3551c5cf97965fa69a9cbd468f9672ab1
Matheus Avellar <matheusavellarb@gmail.com>
2015-07-21 01:38:09 -0300

    Testing new menu interface

--- resources/menu.js
+++ resources/menu.js
@@ -7,14 +7,44 @@ var BCS_MENU =
 +                '<h1>BCS</h1>'
 +                '<span class="bcs-version"></span>'
 +            '</li>'
-+            '<li class="options autowoot">'
-+                '<i class="icon icon-woot-disabled"></i>'
-+                '<span class="autowoot">AutoWoot</span>'
-+                '<div class="bcs-on"></div>'
-+                '<div class="bcs-off active"></div>'
-+            '</li>'
-+            '<ul class="extend">'
-+                '<div class="options-parent">'
++            '<ul class="extend auto">'
++                '<div class="options-parent auto">'
++                    '<i class="icon icon-history-white"></i>'
++                    '<span class="loglist">Auto</span>'
++                '</div>'
++                '<li class="options autowoot options-child">'
++                    '<i class="icon icon-woot"></i>'
++                    '<span class="autowoot">AutoWoot</span>'
++                    '<div class="bcs-on"></div>'
++                    '<div class="bcs-off active"></div>'
++                '</li>'
++                '<li class="options autograb options-child">'
++                    '<i class="icon icon-grab"></i>'
++                    '<span class="autograb">AutoGrab</span>'
++                    '<div class="bcs-on"></div>'
++                    '<div class="bcs-off active"></div>'
++                '</li>'
++                '<li class="options automeh options-child">'
++                    '<i class="icon icon-meh"></i>'
++                    '<span class="automeh">AutoMeh</span>'
++                    '<div class="bcs-on"></div>'
++                    '<div class="bcs-off active"></div>'
++                '</li>'
++                '<li class="options autojoin options-child">'
++                    '<i class="icon icon-join-booth"></i>'
++                    '<span class="autojoin">AutoJoin</span>'
++                    '<div class="bcs-on"></div>'
++                    '<div class="bcs-off active"></div>'
++                '</li>'
++                '<li class="options autoleave options-child">'
++                    '<i class="icon icon-leave-waitlist"></i>'
++                    '<span class="autoleave">AutoLeave</span>'
++                    '<div class="bcs-on"></div>'
++                    '<div class="bcs-off active"></div>'
++                '</li>'
++            '</ul>'
++            '<ul class="extend log">'
++                '<div class="options-parent log">'
 +                    '<i class="icon icon-event-calendar"></i>'
 +                    '<span class="loglist">Logs</span>'
 +                '</div>'
@@ -43,12 +73,6 @@ var BCS_MENU =
 +                    '<div class="bcs-off active"></div>'
 +                '</li>'
 +            '</ul>'
-+            '<li class="options autojoin">'
-+                '<i class="icon icon-join-booth"></i>'
-+                '<span class="autojoin">AutoJoin</span>'
-+                '<div class="bcs-on"></div>'
-+                '<div class="bcs-off active"></div>'
-+            '</li>'
 +            '<li class="options djupdates">'
 +                '<i class="icon icon-soundcloud-tracks small"></i>'
 +                '<span class="djupdates">DJ updates</span>'
@@ -118,8 +142,12 @@ $("div.bcs-on, div.bcs-off").on("click", function(data) {
     bcs.main.utils.settings.set();
 });
 
-$("div#bcs-menu .menu div.options-parent").on("click", function(data) {
-    $("div#bcs-menu .menu ul.extend").toggleClass("open");
+$("div#bcs-menu .menu div.options-parent.log").on("click", function(data) {
+    $("div#bcs-menu .menu ul.extend.log").toggleClass("open");
+});
+
+$("div#bcs-menu .menu div.options-parent.auto").on("click", function(data) {
+    $("div#bcs-menu .menu ul.extend.auto").toggleClass("open");
 });
 
 $("li.clearchat div.bcs-single").on("click", function(data) {
